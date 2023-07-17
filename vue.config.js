c8d94f39ff29f0ee738309136468438736d98ad5
楸枰 <5331659+gslqiuping@user.noreply.gitee.com>
2023-07-17 22:24:14 +0800

    系统布局

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
 });
